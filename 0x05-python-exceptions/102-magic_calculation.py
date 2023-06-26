eff2a1c20d56095c02799ae474c2815c9903e541
Abdoul54 <abdelwahed.akhechane@gmail.com>
2023-06-26 23:42:21 +0100

    n

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
+                raise ValueError('Too far')  # Specify the exception type
             else:
                 result += a ** b / i
-        except:
+        except ValueError:  # Catch the specific exception
             result = b + a
             break
-    return (result)
+    return result
